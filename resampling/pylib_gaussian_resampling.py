33d5dc74e4c26d52596067268c43ce4aa2ff4a48
josephCMY <chanmanyau@gmail.com>
2024-05-23 15:02:14 -0400

    created second variant of gaussian resampling coefficients.

--- resampling/pylib_gaussian_resampling.py
+++ resampling/pylib_gaussian_resampling.py
@@ -1,19 +1,43 @@
 '''
     FUNCTIONS RELATING TO FAST GAUSSIAN RESAMPLING
-    -----------------------------------------------
+    ===============================================
 
     List of functions:
     ------------------
 
-    1) COMPUTE_GAUSSIAN_RESAMPLING_COEFFICIENTS
-       Generates a matrix of resampling coefficients that generates virtual probits with 
-       the same covariance matrix as the forecast members' probits.
+    1) compute_gaussian_resampling_coefficients
+            Generates a matrix of resampling coefficients that generates virtual probits with 
+            the same covariance matrix as the forecast members' probits.
 
-    2) 
 
 '''
 
 import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,7 +58,7 @@ import numpy as np
 
     Note that M must be greater than N!
 '''
-def compute_unlocalized_gaussian_resampling_coefficients( N, M, rng_seed ):
+def compute_unlocalized_gaussian_resampling_coefficients( N, M, rng_seed=0 ):
 
     # Seed the NumPy random number generator -- useful for parallelization.
     np.random.seed(rng_seed)
@@ -76,8 +100,82 @@ def compute_unlocalized_gaussian_resampling_coefficients( N, M, rng_seed ):
 
 
 
+'''
+    FUNCTION TO GENERATE GAUSSIAN RESAMPLING COEFFICIENTS 
+
+    Based on M.-Y. Chan et al 2020 Monthly Weather Review paper on Bi-Gaussian 
+    EnKFs
+
+    Inputs:
+    1) N        -- Original ensemble size
+    2) M        -- Number of additional members
+    3) rng_seed -- Random number generator seed
+
+    Note that M must be greater than N!
+'''
+def IID_compute_unlocalized_gaussian_resampling_coefficients( N, M, rng_seed=0 ):
+
+    # Seed the NumPy random number generator -- useful for parallelization.
+    np.random.seed(rng_seed)
+
+    # Generating matrix of noise (Appendix B)
+    W = np.matrix( np.random.normal( size=[N,M] ) / np.sqrt(M) )
+
+    # Appendix B Step 5
+    k = np.sqrt( 
+                ( M+N-1. )/(N-1.)
+                )
+    C_E = np.eye(N)* M/(N-1.)
+    C_E -= (k-1)**2/M
+    L_E = np.matrix(np.linalg.cholesky( C_E ))
+    
+    # Appendix B Step 6
+    E_prime = L_E * W
+
+    # Appendix B Step 7
+    E = E_prime + (k-1)/M
+
+    return np.matrix( E )
+
+
+
+
+
+
+
+
+
+
 
 
 '''
-    FUNCTION TO GENERATE LOCALIZED GAUSSIAN RESAMPLING COEFFICIENTS OVER
+    Sanity check
 '''
+if __name__ == '__main__':
+    
+    from scipy.ndimage import gaussian_filter1d  
+
+    original_ens = gaussian_filter1d( np.random.normal( size = [100, 10] ), sigma = 10., axis= 0 ) * 6
+    original_mean = np.mean( original_ens, axis=1 )
+    original_perts = (original_ens.T - original_mean).T
+
+    print( np.cov( original_ens[10:12] ))
+
+    resampling_matrix = compute_unlocalized_gaussian_resampling_coefficients( 10, 20000 )
+
+    expanded_ens = np.zeros( (100,10+20000) ) 
+    expanded_ens[:,:10] = original_ens
+    expanded_ens[:,10:] = ( ( np.matrix( original_perts ) * np.matrix( resampling_matrix ) ).T + original_mean ).T
+
+    print('')
+    print( np.cov( expanded_ens[10:12] ))
+
+    resampling_matrix = IID_compute_unlocalized_gaussian_resampling_coefficients( 10, 20000 )
+    expanded_ens[:,:10] = original_ens
+    expanded_ens[:,10:] = ( ( np.matrix( original_perts ) * np.matrix( resampling_matrix ) ).T + original_mean ).T
+    
+    print('')
+    print( np.cov( expanded_ens[10:12] ))
+
+
+    
