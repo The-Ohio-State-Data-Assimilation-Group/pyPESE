8dd017e05da5c490e3a79a709a0e9977556f1ede
josephCMY <chanmanyau@gmail.com>
2024-05-23 12:09:23 -0400

    multiple changes: 1) added function descriptions 2) added function to evaluate BRH CDF, inverse CDF and PDF

--- distributions/bounded_rank_histogram_main.py
+++ distributions/bounded_rank_histogram_main.py
@@ -7,7 +7,18 @@
     The width of the box-car tails is currently set to (maxval - minval) / num_data
     where minval is the data sample's minimum value, and maxval is the data sample's 
     maximum value.
-    
+
+
+    List of functions defined here:
+    --------------------------------
+    1) fit_brh_dist
+            Fits BRH distribution to univariate samples
+    2) eval_brh_cdf
+            Evaluates the CDF of the bounded rank histogram distribution
+            Note: Must first fit the BRH distribution
+    3) eval_brh_inv_cdf
+            Evaluates the inverse CDF of the bounded rank histogram distribution
+            Note: Must first fit the BRH distribution
 '''
 
 
@@ -17,59 +28,60 @@ import numpy as np
 
 
 '''
-    FUNCTION TO FIT BRH TO UNIVARIATE SAMPLES
+    FUNCTION TO FIT BOUNDED RANK HISTOGRAM TO UNIVARIATE SAMPLES
 
     
-    Mandatory Inputs:
+    Mandatory Arguments:
     -----------------
     1) data1d
         1D NumPy array containing data samples
 
         
-    Optional Inputs (i.e., kwargs):
-    -------------------------------
+    Optional Arguments (i.e., kwargs):
+    ----------------------------------
     1) exterior_scaling (default value: 1.0)
-        Controls the probability assigned to the boxcar tails.
-        Probability assigned to one boxcar tail is:
-            exterior_scaling / number_of_samples
+            Controls the probability assigned to the boxcar tails.
+            Probability assigned to one boxcar tail is:
+                exterior_scaling / number_of_samples
 
     2) left_bound (default value: None)
-        Allows user to manually specify the left boundary of the BRH 
-        distribution's support.
-        If left_bound is set to None, the left boundary is:
-            minval - ( maxval - minval ) / number_of_samples
-        If left_bound is a float value and minval > left_bound, then the
-        left boundary is set to left_bound.
-        If minval < left_bound, the left boundary is set to:
-            minval - ( maxval - minval ) / number_of_samples
+            Allows user to manually specify the left boundary of the BRH 
+            distribution's support.
+            If left_bound is set to None, the left boundary is:
+                minval - ( maxval - minval ) / number_of_samples
+            If left_bound is a float value and minval > left_bound, then the
+            left boundary is set to left_bound.
+            If minval < left_bound, the left boundary is set to:
+                minval - ( maxval - minval ) / number_of_samples
     
     3) right_bound (default value: None)
-        Allows users to manually specify the right boundary of the BRH
-        distribution's support.
-        If right_bound is set to None, the right boundary is:
-            maxval + ( maxval - minval ) / number_of_samples
-        If right_bound is a float value and maxval < right_bound, then the
-        right boundary is set to right_bound.
-        If maxval > right_bound, then the right boundary is set to:
-            maxval + ( maxval - minval ) / number_of_samples
+            Allows users to manually specify the right boundary of the BRH
+            distribution's support.
+            If right_bound is set to None, the right boundary is:
+                maxval + ( maxval - minval ) / number_of_samples
+            If right_bound is a float value and maxval < right_bound, then the
+            right boundary is set to right_bound.
+            If maxval > right_bound, then the right boundary is set to:
+                maxval + ( maxval - minval ) / number_of_samples
 
 
     Outputs:
     --------
     1) brh_pts
-        Locations defining the piecewise linear CDF of the BRH distribution.
+            Locations defining the piecewise linear CDF of the BRH distribution.
+
     2) brh_cdf
-        CDF values of the BRH distribution at locations brh_pts.
+            CDF values of the BRH distribution at locations brh_pts.
 '''
 
-def fit_bounded_rank_histogram( data1d, exterior_scaling = 1.0, left_bound = None, right_bound = None ):
+def fit_brh_dist( data1d, exterior_scaling = 1.0, left_bound = None, right_bound = None ):
 
     # Number of data points
     nPts = len( data1d )
 
     # Exterior scaling must be within (0,1].
     if ( exterior_scaling > 1 ) or ( exterior_scaling <= 0 ):
-        print("ERROR: FIT_BOUNDED_RANK_HISTOGRAM_DISTRIBUTION")
+        print("ERROR: fit_brh_dist")
         print("    Exterior scaling factor must be between 0 and 1.")
 
 
@@ -113,6 +125,127 @@ def fit_bounded_rank_histogram( data1d, exterior_scaling = 1.0, left_bound = Non
     brh_cdf[-1] = 1.
 
     return brh_pts, brh_cdf
+
+
+
+
+
+
+
+
+
+
+'''
+    FUNCTION TO EVALUATE BRH CDF
+
+    Mandatory Arguments:
+    --------------------
+    1) eval_pts
+            Locations to evaluate the BRH CDF
+    
+    2) brh_pts
+            Locations defining the piecewise linear BRH CDF
+    
+    3) brh_cdf
+            BRH CDF values at locations brh_pts
+
+'''
+def eval_brh_cdf( eval_pts, brh_pts, brh_cdf ):
+
+    return np.interp( eval_pts, brh_pts, brh_cdf, left=0., right=1.)
+
+
+
+
+
+
+
+
+
+
+'''
+    FUNCTION TO EVALUATE BRH QUANTILE FUNCTION
+    This function applies the inverse of the BRH CDF.
+
+    Mandatory Arguments:
+    --------------------
+    1) eval_cdfs
+            Quantiles to apply the BRH inverse CDF on.
+    
+    2) brh_pts
+            Locations defining the piecewise linear BRH CDF
+    
+    3) brh_cdf
+            BRH CDF values at locations brh_pts
+
+'''
+def eval_brh_inv_cdf( eval_cdf, brh_pts, brh_cdf ):
+    
+    return np.interp( eval_cdf, brh_cdf, brh_pts )
+
+
+
+
+
+
+
+'''
+    FUNCTION TO EVALUATE BRH PROBABILITY DENSITY FUNCTION (PDF)
+    This function uses the centered difference method on the BRH CDF
+    to estimate the BRH PDF at specified locations
+
+    Mandatory Arguments:
+    --------------------
+    1) eval_pts
+            Locations to evaluate the BRH PDF 
+    
+    2) brh_pts
+            Locations defining the piecewise linear BRH CDF
+    
+    3) brh_cdf
+            BRH CDF values at locations brh_pts
+'''
+def eval_brh_pdf( eval_pts, brh_pts, brh_cdf ):
+
+    # Interval used to estimate BRH PDF values
+    interval = (brh_pts[1:] - brh_pts[:-1]).min() * 1e-3
+
+    # Identify left and right points used to evaluate 
+    left_pts  = eval_pts - interval/2.
+    right_pts = eval_pts + interval/2.
+
+    # Evaluate CDF at left and right points
+    left_cdf  = eval_brh_cdf(  left_pts, brh_pts, brh_cdf )
+    right_cdf = eval_brh_cdf( right_pts, brh_pts, brh_cdf )
+
+    # Evaluate PDF via centered difference
+    return (right_cdf - left_cdf) / interval
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
