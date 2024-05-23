1cc99f42d9bb93f5e91d61aa9e778d54281600af
josephCMY <chanmanyau@gmail.com>
2024-05-23 19:19:46 -0400

    made nice demonstrative plots of pyPESE

--- demo_pyPESE.py
+++ demo_pyPESE.py
@@ -11,7 +11,7 @@ from scipy.stats import skewnorm, gamma, norm
 
 # Import pyPESE's Gaussian resampling scheme
 from pyPESE.pese_gc import pese_gc
-# from pyPESE.resampling.gaussian_resampling import fast_unlocalized_gaussian_resampling 
+from pyPESE.distributions.bounded_rank_histogram import bounded_rank_histogram
 
 
 
@@ -34,77 +34,59 @@ original_ens = norm.cdf( original_probits )
 
 # Map from quantile space to native space
 original_ens[0,:] = gamma(3).ppf( original_ens[0,:] )
-original_ens[1,:] = skewnorm(-5).ppf( original_ens[1,:])
+original_ens[1,:] = skewnorm(-10).ppf( original_ens[1,:])
 
 
 
 
 
 '''
-    Now apply PESE-GC!
+    TWO EXAMPLES OF APPLYING PESE-GC
 '''
 
+# Example 1: Employ user-informed distributions
+# ---------------------------------------------
 # Init list of distribution classes
 list_dist_classes = [gamma, skewnorm]
-virtual_ensemble = pese_gc( original_ens, list_dist_classes, ens_size_virtual, rng_seed=0 )
+virtual_ensemble1 = pese_gc( original_ens, list_dist_classes, ens_size_virtual, rng_seed=0 )
+
+
+# Example 2: Employ non-parametric distribution (bounded rank histogram)
+# ----------------------------------------------------------------------
+list_dist_classes = [bounded_rank_histogram, bounded_rank_histogram]
+virtual_ensemble2 = pese_gc( original_ens, list_dist_classes, ens_size_virtual, rng_seed=0 )
 
 
 
+# Plot both examples
+# -------------------
+fig, axs = plt.subplots( ncols=1, nrows=2, figsize=(6,6) )
+
+# Plot out example 1
+axs[0].scatter( original_ens[0,:], original_ens[1,:], c='dodgerblue', 
+             marker='o', label = 'Original Ensemble', s=50)
+axs[0].scatter( virtual_ensemble1[0,:], virtual_ensemble1[1,:], c='r',
+             marker='o', label = 'Virtual Ensemble', s = 1)
+axs[0].set_title('Example 1: User-informed distributions used', loc='left')
+axs[0].legend()
 
 
+# Plot out example 2
+axs[1].scatter( original_ens[0,:], original_ens[1,:], c='dodgerblue', 
+             marker='o', label = 'Original Ensemble', s=50)
+axs[1].scatter( virtual_ensemble2[0,:], virtual_ensemble2[1,:], c='r',
+             marker='o', label = 'Virtual Ensemble', s = 1)
+axs[1].set_title('Example 2: Non-parametric distributions used', loc='left')
+axs[1].legend()
 
 
-
-# '''
-#     Now apply PESE-GC!
-# '''
-
-# # Step 1: Fit distributions
-# dist_list = [None, None]
-
-# shape1, loc1, scale1 = gamma.fit( original_ens[0,:] )
-# dist_list[0] = gamma( shape1, loc=loc1, scale=scale1)
-
-# shape2, loc2, scale2 = skewnorm.fit( original_ens[1,:] )
-# dist_list[1] = skewnorm( shape2, loc=loc2, scale=scale2 )
-# # print( shape2)
+# Make plots pretty
+for i in range(2):
+    axs[i].set_xlabel('Model variable 1')
+    axs[i].set_ylabel('Model variable 2')
 
 
-# # Step 2: transform to probit space
-# fcst_probits = np.zeros( (2, ens_size_original) )
-# for i in range(2):
-#     fcst_probits[i,:] = dist_list[i].cdf( original_ens[i,:] )
-#     fcst_probits[i,:] = norm.ppf( fcst_probits[i,:] )
-#     fcst_probits[i,:] -= np.mean( fcst_probits[i] )
-#     fcst_probits[i,:] /= np.std( fcst_probits[i], ddof=1 )
+fig.subplots_adjust( hspace=0.4)
 
-
-
-
-
-# # Step 3: fast gaussian resampling to cosntruct virtual probits
-# virt_probits = fast_unlocalized_gaussian_resampling(
-#     fcst_probits, ens_size_virtual, rng_seed = 0
-# )
-
-# print( np.cov( fcst_probits) )
-# print( np.cov( virt_probits) )
-
-
-# # Step 4: Invert the PPI transforms on the virtual probits
-# virtual_ensemble = np.zeros( (2, ens_size_virtual) )
-# for i in range(2):
-#     virtual_ensemble[i,:] = norm.cdf( virt_probits[i,:] )
-#     virtual_ensemble[i,:] = dist_list[i].ppf( virtual_ensemble[i,:] )
-
-
-
-
-# Plot data points
-plt.scatter( original_ens[0,:], original_ens[1,:], c='dodgerblue', 
-             marker='o', label = 'Original', s=50)
-plt.scatter( virtual_ensemble[0,:], virtual_ensemble[1,:], c='r',
-             marker='o', label = 'Virtual', s = 1)
-plt.legend()
 
 plt.savefig('demo.png')
