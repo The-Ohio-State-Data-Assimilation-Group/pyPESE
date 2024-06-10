4610991d932a7a3927c6e91d3cee09aa6a75099e
josephCMY <chanmanyau@gmail.com>
2024-06-10 13:32:10 -0400

    drafted function to setup brh-defining cdfs

--- pyPESE/distributions/boxcar_rank_histogram.py
+++ pyPESE/distributions/boxcar_rank_histogram.py
@@ -26,6 +26,7 @@
 
 
 import numpy as np
+from copy import deepcopy
 # from numba import njit
 # from numba import float64 as nb_f64
 # from numba.types import Tuple as nb_tuple
@@ -50,51 +51,51 @@ import numpy as np
 
 
 
-'''
-    FUNCTION TO FIT BOXCAR RANK HISTOGRAM TO UNIVARIATE SAMPLES
+# '''
+#     FUNCTION TO FIT BOXCAR RANK HISTOGRAM TO UNIVARIATE SAMPLES
     
-    This function uses a moment-matching scheme to ascertain the boxcar tail widths
-    and boxcar tail probability masses.
+#     This function uses a moment-matching scheme to ascertain the boxcar tail widths
+#     and boxcar tail probability masses.
 
     
-    Mandatory Arguments:
-    -----------------
-    1) raw_data1d
-            1D NumPy array containing data samples
+#     Mandatory Arguments:
+#     -----------------
+#     1) raw_data1d
+#             1D NumPy array containing data samples
                 
-    Outputs:
-    --------
-    1) brh_pts
-            Locations defining the piecewise linear CDF of the BRH distribution.
+#     Outputs:
+#     --------
+#     1) brh_pts
+#             Locations defining the piecewise linear CDF of the BRH distribution.
 
-    2) brh_cdf
-            CDF values of the BRH distribution at locations brh_pts.
-'''
-# @njit( nb_tuple((nb_f64[:], nb_f64[:]))( nb_f64[:] ) ) 
-def fit_brh_dist( raw_data1d ): 
+#     2) brh_cdf
+#             CDF values of the BRH distribution at locations brh_pts.
+# '''
+# # @njit( nb_tuple((nb_f64[:], nb_f64[:]))( nb_f64[:] ) ) 
+# def fit_brh_dist( raw_data1d ): 
 
-    # Number of RAW data points
-    num_data = len( raw_data1d )
+#     # Number of RAW data points
+#     num_data = len( raw_data1d )
 
-    # Unbiased estimation of the first 3 central moments of the RAW data
-    moment1 = np.mean(raw_data1d)
-    moment2 = np.var( raw_data1d, ddof=1 )
-    moment3 = np.sum( np.power(raw_data1d - moment1, 3) ) * num_data / ( (num_data-1) * (num_data-2) )
+#     # Unbiased estimation of the first 3 central moments of the RAW data
+#     moment1 = np.mean(raw_data1d)
+#     moment2 = np.var( raw_data1d, ddof=1 )
+#     moment3 = np.sum( np.power(raw_data1d - moment1, 3) ) * num_data / ( (num_data-1) * (num_data-2) )
 
-    # Map data to space where mean is zero
-    data1d = raw_data1d - moment1
+#     # Map data to space where mean is zero
+#     data1d = raw_data1d - moment1
 
-    # Determine unique data points
-    uniq_data1d = np.unique( data1d )
-    num_uniq_data = len( uniq_data1d )
+#     # Determine unique data points
+#     uniq_data1d = np.unique( data1d )
+#     num_uniq_data = len( uniq_data1d )
 
-    # Seek degenerate data values -- this also determines the probability masses in BRH intervals
-    degen_count = np.ones( num_uniq_data, dtype = 'i4')
-    if ( num_uniq_data != num_data ):
-        for i, val in enumerate(uniq_data1d):
-            degen_count[i] = np.sum( data1d == val )
-        # --- End of loop search for degenerate data values
-    # --- End of check for degenerate data values
+#     # Seek degenerate data values -- this also determines the probability masses in BRH intervals
+#     degen_count = np.ones( num_uniq_data, dtype = 'i4')
+#     if ( num_uniq_data != num_data ):
+#         for i, val in enumerate(uniq_data1d):
+#             degen_count[i] = np.sum( data1d == val )
+#         # --- End of loop search for degenerate data values
+#     # --- End of check for degenerate data values
      
 
 
@@ -102,17 +103,17 @@ def fit_brh_dist( raw_data1d ):
 
 
 
-    # Set data average to 0 -- this is just a useful thing to do
-    data1d_avg0 = data1d - moment1
+#     # Set data average to 0 -- this is just a useful thing to do
+#     data1d_avg0 = data1d - moment1
 
-    # Ascertain unique
+#     # Ascertain unique
 
 
     
 
 
 
-    return brh_pts, brh_cdf
+#     return brh_pts, brh_cdf
 
 
 
@@ -172,144 +173,174 @@ def eval_brh_mth_raw_moment( brh_pts, brh_cdf, mom_ord ):
 
     Location where left tail ends is called "left boundary"
     Location where right tail ends is called "right boundary"
-    
+
 '''
-def setup_brh_defining_cdf_vals( uniq_data1d, num_uniq_counts, left_bound, right_bound, left_tail_mass, right_tail_mass ):
+def setup_brh_defining_cdf_vals( uniq_data1d, uniq_cnts1d, left_bound, right_bound, left_tail_mass, right_tail_mass ):
 
-
-    # First, determine probability mass assigned to the span of the unique data points
+    # Determine interior probability mass (assuming boundaries are outside of uniq_data1d's span)
     interior_mass = 1. - (right_tail_mass + left_tail_mass)
+  
+    # Determine empirical distribution probability mass associated with each non-degenerate data point
+    mass_per_point = interior_mass / np.sum(uniq_cnts1d)
+
+    # Determine unique data points that lie within two boundaries
+    flag_within_bnds = ( uniq_data1d > left_bound ) * (uniq_data1d < left_bound)
+    uniq_data_within_bnds = deepcopy( uniq_data1d[flag_within_bnds] )
+    cnts_within_bnds = deepcopy( uniq_cnts1d[flag_within_bnds] )
+
+    # Count number of data points that are either (a) on the left boundary or (b) left of the left boundary
+    flag_outside_left = ( uniq_data1d <= left_bound )
+    cnt_outside_left = np.sum( uniq_cnts1d[flag_outside_left] )
+
+    # Count number of data points that are either (a) on the right boundary or (b) right of the right boundary
+    flag_outside_right = ( uniq_data1d >= right_bound )
+    cnt_outside_right = np.sum( uniq_cnts1d[flag_outside_right] )
+
+    # Locations at which BRH CDF values are defined
+    brh_pts = np.zeros( len(uniq_data_within_bnds)+2, dtype=np.float64 )
+    brh_pts[0] = left_bound
+    brh_pts[1:-1] = uniq_data_within_bnds[:]
+    brh_pts[-1] = right_bound
+
+    # Initialize BRH CDF values
+    brh_cdf = np.zeros( len(brh_pts), dtype=np.float64)
     
-    # Determine probability mass associated with each non-degenerate data point
-    mass_per_point = interior_mass / np.sum(num_uniq_counts)
+    # Leftmost BRH CDF value
+    brh_cdf[0] = cnt_outside_left * mass_per_point
 
-    # Generate BRH-defining locations
-    brh_vals = np.zeros( len(uniq_data1d) + 2 )
+    # Accumulate CDF values within interior (left to right)
+    for i, cnt in enumerate( cnts_within_bnds ):
 
+        # Accumulate probability mass
+        brh_cdf[i+1] = brh_cdf[i] + cnt * mass_per_point
 
-
-
-
-
-
-
-
-
-
-
-
-'''
-    FUNCTION TO EVALUATE BRH CDF
-
-    Mandatory Arguments:
-    --------------------
-    1) eval_pts
-            Locations to evaluate the BRH CDF
+        # Special handling for the left tail mass
+        if i == 0:
+            brh_cdf[i+1] += left_tail_mass
     
-    2) brh_pts
-            Locations defining the piecewise linear BRH CDF
+    # Final CDF value
+    brh_cdf[-1] = 1.
+
+    return brh_pts, brh_cdf
     
-    3) brh_cdf
-            BRH CDF values at locations brh_pts
-
-'''
-# @njit( nb_f64[:](nb_f64[:],nb_f64[:],nb_f64[:])  )
-def eval_brh_cdf( eval_pts, brh_pts, brh_cdf ):
-
-    return np.interp( eval_pts, brh_pts, brh_cdf)
 
 
 
 
 
 
+# '''
+#     FUNCTION TO EVALUATE BRH CDF
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-    FUNCTION TO EVALUATE BRH QUANTILE FUNCTION
-    This function applies the inverse of the BRH CDF.
-
-    Mandatory Arguments:
-    --------------------
-    1) eval_cdfs
-            Quantiles to apply the BRH inverse CDF on.
+#     Mandatory Arguments:
+#     --------------------
+#     1) eval_pts
+#             Locations to evaluate the BRH CDF
     
-    2) brh_pts
-            Locations defining the piecewise linear BRH CDF
+#     2) brh_pts
+#             Locations defining the piecewise linear BRH CDF
     
-    3) brh_cdf
-            BRH CDF values at locations brh_pts
+#     3) brh_cdf
+#             BRH CDF values at locations brh_pts
 
-'''
-# @njit( nb_f64[:](nb_f64[:],nb_f64[:],nb_f64[:])  )
-def eval_brh_inv_cdf( eval_cdf, brh_pts, brh_cdf ):
+# '''
+# # @njit( nb_f64[:](nb_f64[:],nb_f64[:],nb_f64[:])  )
+# def eval_brh_cdf( eval_pts, brh_pts, brh_cdf ):
+
+#     return np.interp( eval_pts, brh_pts, brh_cdf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# '''
+#     FUNCTION TO EVALUATE BRH QUANTILE FUNCTION
+#     This function applies the inverse of the BRH CDF.
+
+#     Mandatory Arguments:
+#     --------------------
+#     1) eval_cdfs
+#             Quantiles to apply the BRH inverse CDF on.
     
-    return np.interp( eval_cdf, brh_cdf, brh_pts )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-    FUNCTION TO EVALUATE BRH PROBABILITY DENSITY FUNCTION (PDF)
-    This function uses the centered difference method on the BRH CDF
-    to estimate the BRH PDF at specified locations
-
-    Mandatory Arguments:
-    --------------------
-    1) eval_pts
-            Locations to evaluate the BRH PDF 
+#     2) brh_pts
+#             Locations defining the piecewise linear BRH CDF
     
-    2) brh_pts
-            Locations defining the piecewise linear BRH CDF
+#     3) brh_cdf
+#             BRH CDF values at locations brh_pts
+
+# '''
+# # @njit( nb_f64[:](nb_f64[:],nb_f64[:],nb_f64[:])  )
+# def eval_brh_inv_cdf( eval_cdf, brh_pts, brh_cdf ):
     
-    3) brh_cdf
-            BRH CDF values at locations brh_pts
-'''
-# @njit( nb_f64[:](nb_f64[:],nb_f64[:],nb_f64[:])  )
-def eval_brh_pdf( eval_pts, brh_pts, brh_cdf ):
+#     return np.interp( eval_cdf, brh_cdf, brh_pts )
 
-    # Interval used to estimate BRH PDF values
-    interval = (brh_pts[1:] - brh_pts[:-1]).min() * 1e-3
 
-    # Identify left and right points used to evaluate 
-    left_pts  = eval_pts - interval/2.
-    right_pts = eval_pts + interval/2.
 
-    # Evaluate CDF at left and right points
-    left_cdf  = eval_brh_cdf(  left_pts, brh_pts, brh_cdf )
-    right_cdf = eval_brh_cdf( right_pts, brh_pts, brh_cdf )
 
-    # Evaluate PDF via centered difference
-    return (right_cdf - left_cdf) / interval
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# '''
+#     FUNCTION TO EVALUATE BRH PROBABILITY DENSITY FUNCTION (PDF)
+#     This function uses the centered difference method on the BRH CDF
+#     to estimate the BRH PDF at specified locations
+
+#     Mandatory Arguments:
+#     --------------------
+#     1) eval_pts
+#             Locations to evaluate the BRH PDF 
+    
+#     2) brh_pts
+#             Locations defining the piecewise linear BRH CDF
+    
+#     3) brh_cdf
+#             BRH CDF values at locations brh_pts
+# '''
+# # @njit( nb_f64[:](nb_f64[:],nb_f64[:],nb_f64[:])  )
+# def eval_brh_pdf( eval_pts, brh_pts, brh_cdf ):
+
+#     # Interval used to estimate BRH PDF values
+#     interval = (brh_pts[1:] - brh_pts[:-1]).min() * 1e-3
+
+#     # Identify left and right points used to evaluate 
+#     left_pts  = eval_pts - interval/2.
+#     right_pts = eval_pts + interval/2.
+
+#     # Evaluate CDF at left and right points
+#     left_cdf  = eval_brh_cdf(  left_pts, brh_pts, brh_cdf )
+#     right_cdf = eval_brh_cdf( right_pts, brh_pts, brh_cdf )
+
+#     # Evaluate PDF via centered difference
+#     return (right_cdf - left_cdf) / interval
 
 
 
@@ -402,25 +433,19 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from scipy.stats import norm
     
-    samples = np.random.normal(size=500)
+    # Draw some values
+    samples = np.random.normal(size=5) 
 
-    brh_pts, brh_cdf = boxcar_rank_histogram.fit(samples)
-    brh_dist = boxcar_rank_histogram( brh_pts, brh_cdf)
-    
-    many_pts = np.linspace( -4,4, 1000 )
-    pdf_vals = brh_dist.pdf( many_pts )
-    cdf_vals = brh_dist.cdf( many_pts )
+    # Setup degenerate values
+    samples[-1] = samples[0]
 
+    # Unique values
+    uniq_data, uniq_cnts = np.unique( samples, return_counts=True)
 
-    # Plot PDF and CDF
-    fig, axs = plt.subplots( nrows=1, ncols=2, figsize = (6,3) )
-    axs[0].plot( many_pts, pdf_vals, '-r')
-    axs[0].set_title('BRH PDF')
-    axs[1].plot( many_pts, cdf_vals, '-r')
-    axs[1].set_title('BRH CDF')
+    # Setup BRH
+    brh_pts, brh_cdf = setup_brh_defining_cdf_vals( uniq_data, uniq_cnts, left_bound=1.2, right_bound=1.2, left_tail_mass=0.1, right_tail_mass=0.1 )
 
-    # Overlay with actual CDF
-    gaussian_cdf = norm.cdf( many_pts )
-    axs[1].plot( many_pts, gaussian_cdf, ':k' )
-    plt.savefig('visualize_brh.png')
-    plt.close()
+    # Plot out CDF!
+    plt.plot( brh_pts, brh_cdf, '-r')
+    plt.ylim([0,1])
+    plt.savefig('tmp.png')
