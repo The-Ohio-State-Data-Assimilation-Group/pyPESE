641e7f3b3844f36e12d6660d534f4fe553a76219
josephCMY <chanmanyau@gmail.com>
2024-07-31 16:05:57 -0400

    attempted some amount of geostrophy debugging. not good for tropics though...

--- pyPESE/balance_diagnosis/geostrophy.py
+++ pyPESE/balance_diagnosis/geostrophy.py
@@ -107,7 +107,7 @@ jit_cache_flag = False
     8) lat1d (lat)
             1D NumPy array of latitude values (in degrees).
 '''
-# @njit( nbtuple( (float64[:,:,:], float64[:,:,:], float64[:,:,:]) )( float64[:,:,:], float64[:,:], float64[:,:], float64[:,:,:], float64[:,:], float64[:,:], float64[:], float64[:]) )
+#njit( nbtuple( (float64[:,:,:], float64[:,:,:], float64[:,:,:]) )( float64[:,:,:], float64[:,:], float64[:,:], float64[:,:,:], float64[:,:], float64[:,:], float64[:], float64[:]) )
 def diagnose_geostrophic_flow( pres3d, psurf2d, ptop2d, hgt3d, terrain2d, hgttop2d, lon1d, lat1d ):
 
 
@@ -333,9 +333,12 @@ def SANITY_CHECK_geostrophic_flow_diagnosis():
 #njit( float64[:,:,:]( float64[:,:,:], float64[:,:,:], float64[:,:], float64[:,:] ) )
 def compute_vorticity( uwind3d, vwind3d, lon1d, lat1d ):
 
+    plon1d, plat1d, puwind3d = pad_field_due_to_spherical_symmetry( uwind3d, lon1d, lat1d )
+    plon1d, plat1d, pvwind3d = pad_field_due_to_spherical_symmetry( vwind3d, lon1d, lat1d )
+
     vort3d = ( 
-        compute_df_dx_on_eta_surface( vwind3d, lon1d, lat1d )
-        - compute_df_dy_on_eta_surface( uwind3d, lon1d, lat1d )
+        compute_df_dx_on_eta_surface( pvwind3d, plon1d, plat1d )[1:-1,1:-1,:]
+        - compute_df_dy_on_eta_surface( puwind3d, plon1d, plat1d )[1:-1,1:-1,:]
     )
 
     return vort3d
@@ -366,13 +369,9 @@ def compute_vorticity( uwind3d, vwind3d, lon1d, lat1d ):
 '''
 def compute_streamfunc( uwind3d, vwind3d, lon1d, lat1d ):
 
-    # Handy constant
-    EARTH_RADIUS = 6371*1000 # in meters
-
     # Array dimensions
     nlon, nlat, nlvl = uwind3d.shape
-    ndeg = int(nlat/2)
-
+    
     # First, compute vorticity on lat-lon grid
     vort3d = compute_vorticity( uwind3d, vwind3d, lon1d, lat1d )
 
@@ -478,11 +477,13 @@ def compute_geostrophic_heights_from_horizontal_flow( uwind3d, vwind3d, lon1d, l
     nlon, nlat, nlvl = uwind3d.shape
 
 
-    # Generate coriolis parameter and gradient (i.e., beta)
+    # Generate coriolis parameter 
     latmesh, lonmesh = np.meshgrid( lat1d, lon1d)
     coriolis_param3d = np.empty( uwind3d.shape, dtype='f8' )
     for k in range( nlvl ):
         coriolis_param3d[:,:,k] =  2 * EARTH_ANGULAR_SPEED * np.sin( DEG_2_RAD * latmesh )
+
+    # Generate coriolis parameter gradient
     coriolis_beta3d = compute_df_dy_on_eta_surface( coriolis_param3d, lon1d, lat1d )
 
 
@@ -491,20 +492,21 @@ def compute_geostrophic_heights_from_horizontal_flow( uwind3d, vwind3d, lon1d, l
 
 
     # Compute coriolis term in geostrophic poisson equation
-    coriolis_terms = (
-        compute_d2f_dx2_on_eta_surface( streamfunc3d, lon1d, lat1d )
-        + compute_d2f_dy2_on_eta_surface( streamfunc3d, lon1d, lat1d )
-    ) 
-    coriolis_terms += ( 
-        compute_df_dy_on_eta_surface( streamfunc3d, lon1d, lat1d) 
-        * (coriolis_beta3d / coriolis_param3d)
-    )
-    coriolis_terms *= coriolis_param3d
-
+    coriolis_terms = compute_vorticity( uwind3d, vwind3d, lon1d, lat1d ) * coriolis_param3d
+    # (
+    #     compute_d2f_dx2_on_eta_surface( streamfunc3d, lon1d, lat1d )
+    #     + compute_d2f_dy2_on_eta_surface( streamfunc3d, lon1d, lat1d )
+    # ) 
+    # coriolis_terms += ( 
+    #     compute_df_dy_on_eta_surface( streamfunc3d, lon1d, lat1d) 
+    #     * (coriolis_beta3d / coriolis_param3d)
+    # )
+    # coriolis_terms *= coriolis_param3d
+    
 
     # Invert Poisson equation to obtain mean-zero geopotential field
     zero_mean_geopot3d = spherical_invert_poisson_equation( coriolis_terms )
-    zero_mean_geopot3d -= np.mean( np.mean( zero_mean_geopot3d, axis=0), axis=1 )
+    zero_mean_geopot3d -= np.mean( np.mean( zero_mean_geopot3d, axis=0), axis=0 )
 
 
     # Return mean-zero geopotential heights
@@ -550,29 +552,31 @@ def compute_geostrophic_heights_on_eta_lvls( uwind3d, vwind3d, pres3d, hgt3d, ps
 
     # Detect dimensions
     nlon, nlat, neta = uwind3d.shape
-    nplvl = 201
+    nplvl = 101
 
     # Useful constants
     GRAVITY_ACCEL = 9.80665 
     
     # Pressure layers to do calculations on 
-    plvls1d = np.linspace( pres3d.min(), 1.01e5, nplvl)[::-1]
+    plvls1d = np.linspace( pres3d.min(), pres3d.max(), nplvl)[::-1]
 
     # Interpolate U & V to desired pressure levels
     uwind_plvl = basic_interpolate_to_pressure_levs( pres3d, uwind3d, plvls1d)
     vwind_plvl = basic_interpolate_to_pressure_levs( pres3d, vwind3d, plvls1d)
 
+
     # Interpolate geopotential to pressure levels
     hgt_plvl = interp_geopotential_to_plvls( 
-        hgt3d*GRAVITY_ACCEL, pres3d, psurf2d, tsurf2d, terrain2d, plvls1d
+        hgt3d*GRAVITY_ACCEL, pres3d, psurf2d, tsurf2d, terrain2d, plvls1d.astype('f8')
     )
     hgt_plvl /= GRAVITY_ACCEL
+
 
     # Compute geostrophic heights with mean zero
     mean_zero_hgt = compute_geostrophic_heights_from_horizontal_flow( uwind_plvl, vwind_plvl, lon1d, lat1d )
 
     # Adjust the layerwise mean of the geostrophic height
-    geostrophic_hgt_plvl = mean_zero_hgt + np.mean( np.mean( hgt_plvl, axis=0), axis=1)
+    geostrophic_hgt_plvl = mean_zero_hgt + np.mean( np.mean( hgt_plvl, axis=0), axis=0)
 
     # Interpolate geostrophic heights from plvls back to eta lvls
     geostrophic_hgt3d = basic_interpolate_to_eta_levs( plvls1d, geostrophic_hgt_plvl, pres3d )
