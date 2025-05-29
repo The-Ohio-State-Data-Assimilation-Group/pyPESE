d31c1da5c82be0abfb2f53353c96415bc93c3ac2
JosephChan <chan.1063@osu.edu>
2025-05-29 15:46:26 -0400

    defined class for muwe

--- pyPESE/distributions/mixture_user_and_weighted_empirical.py
+++ pyPESE/distributions/mixture_user_and_weighted_empirical.py
@@ -39,142 +39,66 @@ import numpy as np
 from copy import deepcopy
 from numba import njit
 from numba import float64 as nb_f64
-# from numba import float32 as nb_f32
-# from numba import int64 as nb_i64
-# from numba.types import Tuple as nb_tuple
 
 
 
 
-# '''
-#     Class defining the delta+user mixture distribution
+'''
+    Class defining the mixture user + weighted empirical distribution
 
-#     Note: This function is initialized using other pyPESE distribution classes.
-# '''
-# class mixture_user_and_delta: 
+    Note: This function is initialized using SciPy-like distribution classes.
+'''
+class mixture_user_weighted_empirical: 
 
-#     name = 'generic delta-user mixture distribution'
+    name = 'generic mixture user + weighted empirical distribution'
 
+    # Initialization
+    def __init__( self, delta_pts, delta_weights, user_dist_inst, user_weight):
+        self.delta_pts      = delta_pts
+        self.delta_weights  = delta_weights
+        self.user_dist      = user_dist_inst
+        self.user_weight    = 1. - np.sum(delta_weights)   
+        return  
 
-#     # Initialize 
-#     # Note: user_dist_class must be a class, not an instance!!!
-#     def __init__( self, ens_values1d, user_dist_class ):
-
-#         # Error-handling variables
-#         self.err_flag = False
-#         self.err_msg = ''
-
-#         # Error check: user_dist_class is an instance, not a class
-#         if isinstance( user_dist_class ):
-#             self.err_flag = True
-#             self.err_msg += '\nERROR (pyPESE . distributions . mixture_user_and_delta . mixture_user_and_delta . __init__):\n'
-#             self.err_msg += 'user_dist_class used to init mixture_user_and_delta is an instance!\n'
-#             self.err_msg += 'Name of the inputted user_dist_class instance: %s\n' % user_dist_class.name
-#         # --- End of class checking
-
-#         # Sort ensemble values
-#         sorted_ens_vals = np.sort( ens_values1d )
-#         ens_size = len( sorted_ens_vals )
-#         self.ens_vals = sorted_ens_vals
-
-#         # Detect unique values 
-#         vals, cnt = np.unique( sorted_ens_vals, return_counts = True)
-#         uniq_vals = vals[cnt == 1]
-
-#         # Detect degenerate values & their frequencies
-#         degen_vals = vals[cnt > 1]
-#         degen_freq = cnt[cnt>1]
-
-#         # -----------------------------------------------------------------
-#         # Fit & init user-specified distribution
-#         # -----------------------------------------------------------------
-
-#         # Fit user-specified distribution to unique values
-#         params = user_dist_class.fit( uniq_vals )
-
-#         # Store user-specified distribution instance
-#         self.user_dist_instance = user_dist_class( *params )
-#         self.user_dist_weight = len( uniq_vals ) / ens_size
-
-
-#         # -----------------------------------------------------------------
-#         # Prepare delta distribution information
-#         # -----------------------------------------------------------------
-#         self.delta_dist_weight = ( ens_size - len( uniq_vals ) ) / ens_size 
-
-#         # Only prep info if delta distribution is activate
-#         if self.delta_dist_weight > 0:
-#             self.delta_dist_vals = degen_vals
-#             self.delta_dist_cnts = degen_freq
-
-#             # Eval CDF at ensemble values
-#             self.delta_dist_ens_cdf = np.zeros_like( sorted_ens_vals )
-#             self.num_degen_vals = np.sum( degen_freq )
-#             counter = 0
-#             for imem in range( ens_size ):
-#                 # Increment CDF for degen values
-#                 if ( self.ens_vals[imem] in self.delta_dist_vals ):
-#                     counter += 1
-#                 # Eval CDF
-#                 self.delta_dist_ens_cdf[imem] = counter/self.num_degen_vals
-#             # --- End of CDF evaluation loop
-#         # --- End of delta distribution information prep
-
-
-#         # Check: Do weights sum to unity?
-#         checksum = self.delta_dist_weight + self.user_dist_weight
-#         if ( np.abs( checksum - 1) > 1e-6 ) :
-#             self.err_flag = True
-#             self.err_msg += '\nERROR (pyPESE . distributions . mixture_user_and_delta . mixture_user_and_delta . __init__): \n'
-#             self.err_msg += 'Sum of weights (%f) is not unity!\n' % checksum
-#         # --- End of weight sum check
-
-
-#         return
-#     # --- End of definition for mixture_user_and_delta class
-
-
-    
-
-#     # Function to evaluate CDF of mixture_user_and_delta distribution
-#     def cdf( self, eval_pts ):
-
-#         # Evaluate CDF of user component
-#         cdf_user = self.user_dist_instance.cdf( eval_pts )
-
-#         # Evaluate CDF of delta component (should component exist)
-#         if self.delta_dist_flag:
-#             cdf_delta = mud_delta_cdf( self, eval_pts )
-#         else:
-#             cdf_delta = np.ones_like( eval_pts )
-#         # --- End of delta distribution eval
-
-#         # Mix the two CDFs
-#         cdf_mix = ( 
-#             cdf_user * self.user_dist_weight 
-#             + cdf_delta * self.delta_dist_weight
-#         )
-
-#         # Return the mixed CDF
-#         return cdf_mix
-#     # --- End of function to evaluate the CDF
-
-
-
-#     # Public-facing function to evaluate inverse CDF (aka, percent point function)
-#     def ppf( self, eval_cdf ):
+    # Fit muwe distribution to 1d ensemble data
+    def fit( data1d, user_dist_class ):
         
-#         # PPF calculation is simple if delta component does not exist.
-#         if self.delta_dist_weight == 0 :
-#             ppf_output = self.user_dist_instance.ppf( eval_cdf )
-#         # --- End of calculation for pure user distribution
+        # Identifying delta points and weights
+        vals, cnt = np.unique( data1d, return_counts=True )
+        delta_pts = vals[ cnt>1 ]
+        delta_weights = cnt[cnt>1] * 1. / len(data1d)
 
-#         # PPF calculation is not trivial for mixed distribution
-#         if self.delta_dist_weight > 0:
-#             ppf_output = mud_ppf( eval_cdf )
+        # Determine user distribution parameters based on non-degen values
+        user_params = user_dist_class.fit( vals[cnt==1] )
 
+        # Generate instance of user distribution
+        user_dist_inst = user_dist_class(*user_params)
 
-# # ------------ End of definition for mixture_and_user_delta distribution SciPy-like class
+        # Return parameters needed to initialize muwe instance
+        return delta_pts, delta_weights, user_dist_inst
+    
+    # CDF function
+    def cdf( self, eval_pts ):
+        return muwe_cdf( self.delta_pts, self.delta_weights, self.user_dist, self.user_weight, eval_pts )
+    
+    # PPF function
+    def ppf( self, eval_cdf ):
+        return muwe_ppf_fast( self.delta_pts, self.delta_weights, self.user_dist, self.user_weight, eval_cdf )
+    
+    # PDF function -- estimated via taking numerical gradient of CDF
+    def pdf( self, eval_pts ):
+        eval_left  = eval_pts * (1 - 1e-4) - 1e-6
+        eval_right = eval_pts * (1 + 1e-4) + 1e-6
+        dx = eval_right - eval_left
+        return (self.cdf( eval_right ) - self.cdf( eval_left ))/dx
+    
+    # Random sampling
+    def rvs( self, shape ):
+        uniform_samples1d = np.random.uniform( size=np.prod(shape) )
+        samples1d = self.ppf( uniform_samples1d )
+        return samples1d.reshape(shape)
+
+# # ------------ End of definition for mixture_user_weighted_empirical distribution SciPy-like class
 
 
 
