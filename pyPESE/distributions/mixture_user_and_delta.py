0d2b766c8f5a59c1cd848c1a1e4ef3782919b4c8
JosephChan <chan.1063@osu.edu>
2025-05-29 13:40:15 -0400

    drafted first seemingly efficientish code for delta cdf eval

--- pyPESE/distributions/mixture_user_and_delta.py
+++ pyPESE/distributions/mixture_user_and_delta.py
@@ -23,146 +23,144 @@
 
 import numpy as np
 from copy import deepcopy
-from numba import njit
-from numba import float64 as nb_f64
-from numba import float32 as nb_f32
-from numba import int64 as nb_i64
-from numba.types import Tuple as nb_tuple
+# from numba import njit
+# from numba import float64 as nb_f64
+# from numba import float32 as nb_f32
+# from numba import int64 as nb_i64
+# from numba.types import Tuple as nb_tuple
 
 
 
 
-'''
-    Class defining the delta+user mixture distribution
+# '''
+#     Class defining the delta+user mixture distribution
 
-    Note: This function is initialized using other pyPESE distribution classes.
-'''
-class mixture_user_and_delta: 
+#     Note: This function is initialized using other pyPESE distribution classes.
+# '''
+# class mixture_user_and_delta: 
 
-    name = 'generic delta-user mixture distribution'
-
-
-    # Initialize 
-    # Note: user_dist_class must be a class, not an instance!!!
-    def __init__( self, ens_values1d, user_dist_class ):
-
-        # Error-handling variables
-        self.err_flag = False
-        self.err_msg = ''
-
-        # Error check: user_dist_class is an instance, not a class
-        if isinstance( user_dist_class ):
-            self.err_flag = True
-            self.err_msg += '\nERROR (pyPESE . distributions . mixture_user_and_delta . mixture_user_and_delta . __init__):\n'
-            self.err_msg += 'user_dist_class used to init mixture_user_and_delta is an instance!\n'
-            self.err_msg += 'Name of the inputted user_dist_class instance: %s\n' % user_dist_class.name
-        # --- End of class checking
-
-        # Sort ensemble values
-        sorted_ens_vals = np.sort( ens_values1d )
-        ens_size = len( sorted_ens_vals )
-        self.ens_vals = sorted_ens_vals
-
-        # Detect unique values 
-        vals, cnt = np.unique( sorted_ens_vals, return_counts = True)
-        uniq_vals = vals[cnt == 1]
-
-        # Detect degenerate values & their frequencies
-        degen_vals = vals[cnt > 1]
-        degen_freq = cnt[cnt>1]
-
-        # -----------------------------------------------------------------
-        # Fit & init user-specified distribution
-        # -----------------------------------------------------------------
-
-        # Fit user-specified distribution to unique values
-        params = user_dist_class.fit( uniq_vals )
-
-        # Store user-specified distribution instance
-        self.user_dist_instance = user_dist_class( *params )
-        self.user_dist_weight = len( uniq_vals ) / ens_size
+#     name = 'generic delta-user mixture distribution'
 
 
-        # -----------------------------------------------------------------
-        # Prepare delta distribution information
-        # -----------------------------------------------------------------
-        self.delta_dist_weight = ( ens_size - len( uniq_vals ) ) / ens_size 
+#     # Initialize 
+#     # Note: user_dist_class must be a class, not an instance!!!
+#     def __init__( self, ens_values1d, user_dist_class ):
 
-        # Only prep info if delta distribution is activate
-        if self.delta_dist_weight > 0:
-            self.delta_dist_vals = degen_vals
-            self.delta_dist_cnts = degen_freq
+#         # Error-handling variables
+#         self.err_flag = False
+#         self.err_msg = ''
 
-            # Eval CDF at ensemble values
-            self.delta_dist_ens_cdf = np.zeros_like( sorted_ens_vals )
-            self.num_degen_vals = np.sum( degen_freq )
-            counter = 0
-            for imem in range( ens_size ):
-                # Increment CDF for degen values
-                if ( self.ens_vals[imem] in self.delta_dist_vals ):
-                    counter += 1
-                # Eval CDF
-                self.delta_dist_ens_cdf[imem] = counter/self.num_degen_vals
-            # --- End of CDF evaluation loop
-        # --- End of delta distribution information prep
+#         # Error check: user_dist_class is an instance, not a class
+#         if isinstance( user_dist_class ):
+#             self.err_flag = True
+#             self.err_msg += '\nERROR (pyPESE . distributions . mixture_user_and_delta . mixture_user_and_delta . __init__):\n'
+#             self.err_msg += 'user_dist_class used to init mixture_user_and_delta is an instance!\n'
+#             self.err_msg += 'Name of the inputted user_dist_class instance: %s\n' % user_dist_class.name
+#         # --- End of class checking
+
+#         # Sort ensemble values
+#         sorted_ens_vals = np.sort( ens_values1d )
+#         ens_size = len( sorted_ens_vals )
+#         self.ens_vals = sorted_ens_vals
+
+#         # Detect unique values 
+#         vals, cnt = np.unique( sorted_ens_vals, return_counts = True)
+#         uniq_vals = vals[cnt == 1]
+
+#         # Detect degenerate values & their frequencies
+#         degen_vals = vals[cnt > 1]
+#         degen_freq = cnt[cnt>1]
+
+#         # -----------------------------------------------------------------
+#         # Fit & init user-specified distribution
+#         # -----------------------------------------------------------------
+
+#         # Fit user-specified distribution to unique values
+#         params = user_dist_class.fit( uniq_vals )
+
+#         # Store user-specified distribution instance
+#         self.user_dist_instance = user_dist_class( *params )
+#         self.user_dist_weight = len( uniq_vals ) / ens_size
 
 
-        # Check: Do weights sum to unity?
-        checksum = self.delta_dist_weight + self.user_dist_weight
-        if ( np.abs( checksum - 1) > 1e-6 ) :
-            self.err_flag = True
-            self.err_msg += '\nERROR (pyPESE . distributions . mixture_user_and_delta . mixture_user_and_delta . __init__): \n'
-            self.err_msg += 'Sum of weights (%f) is not unity!\n' % checksum
-        # --- End of weight sum check
+#         # -----------------------------------------------------------------
+#         # Prepare delta distribution information
+#         # -----------------------------------------------------------------
+#         self.delta_dist_weight = ( ens_size - len( uniq_vals ) ) / ens_size 
+
+#         # Only prep info if delta distribution is activate
+#         if self.delta_dist_weight > 0:
+#             self.delta_dist_vals = degen_vals
+#             self.delta_dist_cnts = degen_freq
+
+#             # Eval CDF at ensemble values
+#             self.delta_dist_ens_cdf = np.zeros_like( sorted_ens_vals )
+#             self.num_degen_vals = np.sum( degen_freq )
+#             counter = 0
+#             for imem in range( ens_size ):
+#                 # Increment CDF for degen values
+#                 if ( self.ens_vals[imem] in self.delta_dist_vals ):
+#                     counter += 1
+#                 # Eval CDF
+#                 self.delta_dist_ens_cdf[imem] = counter/self.num_degen_vals
+#             # --- End of CDF evaluation loop
+#         # --- End of delta distribution information prep
 
 
-        return
-    # --- End of definition for mixture_user_and_delta class
+#         # Check: Do weights sum to unity?
+#         checksum = self.delta_dist_weight + self.user_dist_weight
+#         if ( np.abs( checksum - 1) > 1e-6 ) :
+#             self.err_flag = True
+#             self.err_msg += '\nERROR (pyPESE . distributions . mixture_user_and_delta . mixture_user_and_delta . __init__): \n'
+#             self.err_msg += 'Sum of weights (%f) is not unity!\n' % checksum
+#         # --- End of weight sum check
+
+
+#         return
+#     # --- End of definition for mixture_user_and_delta class
 
 
     
 
-    # Function to evaluate CDF of mixture_user_and_delta distribution
-    def cdf( self, eval_pts ):
+#     # Function to evaluate CDF of mixture_user_and_delta distribution
+#     def cdf( self, eval_pts ):
 
-        # Evaluate CDF of user component
-        cdf_user = self.user_dist_instance.cdf( eval_pts )
+#         # Evaluate CDF of user component
+#         cdf_user = self.user_dist_instance.cdf( eval_pts )
 
-        # Evaluate CDF of delta component (should component exist)
-        if self.delta_dist_flag:
-            cdf_delta = mud_delta_cdf( self, eval_pts )
-        else:
-            cdf_delta = np.ones_like( eval_pts )
-        # --- End of delta distribution eval
+#         # Evaluate CDF of delta component (should component exist)
+#         if self.delta_dist_flag:
+#             cdf_delta = mud_delta_cdf( self, eval_pts )
+#         else:
+#             cdf_delta = np.ones_like( eval_pts )
+#         # --- End of delta distribution eval
 
-        # Mix the two CDFs
-        cdf_mix = ( 
-            cdf_user * self.user_dist_weight 
-            + cdf_delta * self.delta_dist_weight
-        )
+#         # Mix the two CDFs
+#         cdf_mix = ( 
+#             cdf_user * self.user_dist_weight 
+#             + cdf_delta * self.delta_dist_weight
+#         )
 
-        # Return the mixed CDF
-        return cdf_mix
-    # --- End of function to evaluate the CDF
+#         # Return the mixed CDF
+#         return cdf_mix
+#     # --- End of function to evaluate the CDF
 
 
 
-    # Public-facing function to evaluate inverse CDF (aka, percent point function)
-    def ppf( self, eval_cdf ):
+#     # Public-facing function to evaluate inverse CDF (aka, percent point function)
+#     def ppf( self, eval_cdf ):
         
-        # PPF calculation is simple if delta component does not exist.
-        if self.delta_dist_weight == 0 :
-            ppf_output = self.user_dist_instance.ppf( eval_cdf )
-        # --- End of calculation for pure user distribution
+#         # PPF calculation is simple if delta component does not exist.
+#         if self.delta_dist_weight == 0 :
+#             ppf_output = self.user_dist_instance.ppf( eval_cdf )
+#         # --- End of calculation for pure user distribution
 
-        # PPF calculation is not trivial for mixed distribution
-        if self.delta_dist_weight > 0:
-            ppf_output = mud_ppf( eval_cdf )
-
-
-# ------------ End of definition for mixture_and_user_delta distribution SciPy-like class
+#         # PPF calculation is not trivial for mixed distribution
+#         if self.delta_dist_weight > 0:
+#             ppf_output = mud_ppf( eval_cdf )
 
 
+# # ------------ End of definition for mixture_and_user_delta distribution SciPy-like class
 
 
 
@@ -188,34 +186,36 @@ class mixture_user_and_delta:
 
 
 
-'''
-    FUNCTION TO EVALUATE CDF OF MIXTURE USER AND DELTA DISTRIBUTION
-
-    Inputs:
-    1) delta_pts        -- Locations of delta functions (a sorted 1D NumPy array)
-    2) delta_weights    -- Weights assigned to each delta function (1D NumPy array)
-    3) user_dist        -- Fitted user-specified SciPy-like distribution instance
-    4) user_weight      -- Weight assigned to the user distribution
-    5) eval_pts         -- 1D NumPy array containing locations at which to evaluate CDF
-
-    NOTE: sum of delta_weights and user_weight must be unity!
-'''
-def mud_delta_cdf( delta_pts, delta_weights, user_dist, user_weight, eval_pts ):
 
 
-    # Delta distribution CDF contribution
-    if len( delta_pts ) > 0:
+# '''
+#     FUNCTION TO EVALUATE CDF OF MIXTURE USER AND DELTA DISTRIBUTION
 
-        # Separating 
+#     Inputs:
+#     1) delta_pts        -- Locations of delta functions (a sorted 1D NumPy array)
+#     2) delta_weights    -- Weights assigned to each delta function (1D NumPy array)
+#     3) user_dist        -- Fitted user-specified SciPy-like distribution instance
+#     4) user_weight      -- Weight assigned to the user distribution
+#     5) eval_pts         -- 1D NumPy array containing locations at which to evaluate CDF
 
-        delta_cdf = np.searchsorted( eval_pts, delta_weights)
+#     NOTE: sum of delta_weights and user_weight must be unity!
+# '''
+# def mud_delta_cdf( delta_pts, delta_weights, user_dist, user_weight, eval_pts ):
+
+
+#     # Delta distribution CDF contribution
+#     if len( delta_pts ) > 0:
+
+#         # Separating 
+
+#         delta_cdf = np.searchsorted( eval_pts, delta_weights)
 
 
 
     
 
 
-    return out_cdf
+#     return out_cdf
 
 
 
@@ -227,7 +227,7 @@ def mud_delta_cdf( delta_pts, delta_weights, user_dist, user_weight, eval_pts ):
     FUNCTION TO EVALUATE CDF OF MULTI-DELTA DISTRIBUTION
 
     Inputs:
-    1) delta_pts                -- Sorted locations of delta functions (1D NumPy array)
+    1) delta_pts                -- Sorted UNIQUE locations of delta functions (1D NumPy array)
     2) delta_weights_normalized -- Weights assigned to each delta function (1D NumPy array)
     3) eval_pts                 -- 1D NumPy array containing locations at which to evaluate CDF
 
@@ -236,86 +236,50 @@ def mud_delta_cdf( delta_pts, delta_weights, user_dist, user_weight, eval_pts ):
         ~ sum of delta_weights should be less than or equal to unity
         ~ borrowed ideas from Jeffrey Anderson to counteract issue of having multiple 
           evaluation points that lie on the same delta point
+        ~ assumes that repeated eval points lie on a delta point
 '''
 def multi_delta_cdf( delta_pts, delta_weights_normalized, eval_pts ):
 
     # Init output array
     out_cdf = np.empty_like( eval_pts)
 
-    # Detecting repeated eval pts -- such pts need to be handled differently
-    # from other (i.e., singular) eval pts
-    uniq_pts, cnt = np.unique( eval_pts, return_counts = True )
-    eval_pts_repeat = uniq_pts[cnt > 1]
-
-    # Sum of all 
+    # Detect delta points that coincide with eval points
+    vals, cnts = np.unique( eval_pts, return_count=True)
+    flag_delta = np.isin( delta_pts, vals )
+    delta_cnts = np.zeros_like( delta_pts )
+    for ipt, pt in enumerate(delta_pts):
+        if flag_delta[ipt]:
+            ind = np.where( vals == pt )[0,0]
+            delta_cnts[ipt] = cnts[ind]
+    # --- End of detection
+    
 
     # Looping over every evaluation point
-    eval_pts_cnt = np.zeros_like( eval_pts_repeat )
+    delta_counter = np.zeros_like( delta_cnts )
     for ipt, pt in enumerate( eval_pts ):
-        
-        # CDF calculation for unique eval points
-        if pt not in eval_pts_repeat:
+
+        # Identify delta locations that this point is to the right of
+        flag_right = pt > delta_pts
+
+        # Evaluate CDF based on delta points to the left of pt
+        out_cdf[ipt] = np.sum( delta_weights_normalized )
+
+        # Special handling if point lies on delta function
+        if pt in delta_pts:
             
+            # Index corresponding to delta_cnts
+            ind = np.where( delta_pts == pt )
 
-
-
-
-
-
-
-
-
-
-
-    # Eval pts that only occurs once (uniq) must be treated separately from other eval pts
-    # Note: np.isin means numpy . is in --- the resulting boolean array is True for eval_pts
-    #       that are within delta_pts
-    vals, eval_pts_cnt = np.unique( eval_pts, return_counts = True)
-    uniq_vals = vals[eval_pts_cnt==1]
-    eval_flag_uniq = np.isin( eval_pts, uniq_vals )
-    eval_pts_uniq = eval_pts[eval_flag_uniq]
-
-
-    # Evaluate CDF for unique locations
-    # ---------------------------------
-    # --- ChatGPT suggestion start
-    cdf_steps = np.cumsum( delta_weights_normalized )
-    cdf_inds_uniq = np.searchsorted( eval_pts_uniq, delta_pts, side='right')
-    cdf_vals_uniq = cdf_steps[cdf_inds_uniq - 1 ]
-    cdf_vals_uniq[ cdf_inds_uniq ==0] = 0.
-    # --- ChatGPT suggestion end
-    out_cdf[eval_flag_uniq] = cdf_vals_uniq
-
-
-    # Evaluate CDF for degenerate locations
-    # -------------------------------------
-    # Isolating degen eval values
-    eval_flag_degen = np.invert( eval_flag_uniq )
-    eval_pts_degen = eval_pts[eval_flag_degen]
-
-    # Useful counter for degenerate CDF evals
-    degen_cnter = np.zeros_like( eval_pts_cnt )
-
-    # Looped search to evaluate 
-    # Init array to hold CDF values
-    cdf_vals_degen = np.empty_like( eval_pts_degen )
-    dege
-    
-    
-
-    # Evaluate CDF at degenerate locations
-
-    cdf_vals_degen = np.empty_like( eval_pts_degen )
-    
-    # Treatment for unique degenerate eval locations
-    vals, cnt = np.unique( cdf_vals_degen, return_counts = True)
-    eval_pts_uniq_degen = vals[cnt == 1]
-    
-
-    
-
-    
-
-    
+            # Increment CDF if eval point lies on delta point
+            delta_counter[ind] += 1
+            out_cdf[ipt] += (
+                delta_weights_normalized
+                * (delta_counter[ind] / delta_cnts[ipt])
+            )
+        # --- End of special treatment
+    # --- End of loop over all eval points
 
     return out_cdf
+
+
+
