5f9348fe909710cb29e5a3b4ee325f2e6c86c551
JosephChan <chan.1063@osu.edu>
2026-09-24 13:15:05 -0400

    finished a first full draft fo ERA5 bpgc code.

--- supported_models/ERA5/generate_BasicPGC_expanded_ensemble.py
+++ supported_models/ERA5/generate_BasicPGC_expanded_ensemble.py
@@ -282,140 +282,145 @@ E_matrix = compute_unlocalized_gaussian_resampling_coefficients_with_precomputed
 for typekey in ['pres', 'single']:
 
     tkey = typekey[0]
-
     vbl_config_dict = {'p': plvl_vbl_config_dict, 's': sngl_vbl_config_dict}[tkey]
 
-    # Check if we want to resample the above kind of data.
-    if ens_config_dict[f'expand {typekey} lvl data?']:
+    # Skip if user doesnt want to expand the current kind of data.
+    if not ens_config_dict[f'expand {typekey} lvl data?']:
+        continue
 
-        virt_ens_dict[f'{tkey}lvl variables'] = {}
+    timed_print(f'Applying PESE-GC onto {typekey} variables.')
 
-        # Loop over available variables
-        for vname in orig_ens_dict[f'{tkey}lvl variables']:
+    virt_ens_dict[f'{tkey}lvl variables'] = {}
 
-            # Duplicate 
-            virt_ens_dict[f'{tkey}lvl variables'][vname] = deepcopy(
-                    orig_ens_dict[f'{tkey}lvl variables'][vname]
-            )
+    # Loop over available variables
+    for vname in orig_ens_dict[f'{tkey}lvl variables']:
 
-            # Skip over variables that are 1D or 0D
-            if orig_ens_dict[f'{tkey}lvl variables'][vname]['data'].ndim <= 2:
-                continue
+        timed_print(f'Applying PESE-GC onto {typekey} variables...')
 
-            # Skip over variables with no corresponding resampling configs
-            if vname not in vbl_config_dict:
-                continue
+        # Duplicate 
+        virt_ens_dict[f'{tkey}lvl variables'][vname] = deepcopy(
+                orig_ens_dict[f'{tkey}lvl variables'][vname]
+        )
 
-            # Detect dimension corresponding to the ensemble member id
-            dim_list = list( orig_ens_dict[f'{tkey}lvl variables'][vname]['dimensions'] )
-            ens_dim_id = dim_list.index( 'number' )
+        # Skip over variables that are 1D or 0D
+        if orig_ens_dict[f'{tkey}lvl variables'][vname]['data'].ndim <= 2:
+            continue
 
-            # Re-order data dimensions s.t. ensemble dimension is last.
-            vble_data = np.swapaxes(
-                orig_ens_dict[f'{tkey}lvl variables'][vname]['data'], ens_dim_id, -1
-            )
+        timed_print(f'... applying PESE-GC onto {typekey} variable {vname}.')
 
-            # Flatten spatial dimensions to simplify loop structure
-            old_dims = deepcopy( vble_data.shape )
-            spatial_dims = deepcopy( vble_data.shape[:-1] )
-            old_ens_size = deepcopy( vble_data.shape[-1] )
-            fcst_vble_data = vble_data.reshape( (np.prod(spatial_dims), old_ens_size) )
+        # Skip over variables with no corresponding resampling configs
+        if vname not in vbl_config_dict:
+            continue
 
-            # Eliminate ensemble members if needed
-            fcst_vble_data = fcst_vble_data[:,:fcst_ens_size]
+        # Detect dimension corresponding to the ensemble member id
+        dim_list = list( orig_ens_dict[f'{tkey}lvl variables'][vname]['dimensions'] )
+        ens_dim_id = dim_list.index( 'number' )
 
-            # Init array to hold virtual memebrs
-            virt_vble_data = np.zeros( (fcst_vble_data.shape[0], virt_ens_size), dtype='f8')
+        # Re-order data dimensions s.t. ensemble dimension is last.
+        vble_data = np.swapaxes(
+            orig_ens_dict[f'{tkey}lvl variables'][vname]['data'], ens_dim_id, -1
+        )
+
+        # Flatten spatial dimensions to simplify loop structure
+        old_dims = deepcopy( vble_data.shape )
+        spatial_dims = deepcopy( vble_data.shape[:-1] )
+        old_ens_size = deepcopy( vble_data.shape[-1] )
+        fcst_vble_data = vble_data.reshape( (np.prod(spatial_dims), old_ens_size) )
+
+        # Eliminate ensemble members if needed
+        fcst_vble_data = fcst_vble_data[:,:fcst_ens_size]
+
+        # Init array to hold virtual memebrs
+        virt_vble_data = np.zeros( (fcst_vble_data.shape[0], virt_ens_size), dtype='f8')
 
 
-            # Purrform PESE-GC
-            for ix in range( vble_data.shape[0] ):
+        # Purrform PESE-GC
+        for ix in range( vble_data.shape[0] ):
 
-                fcst_ens1d = vble_data[ix,:]
+            fcst_ens1d = vble_data[ix,:]
 
-                # Only execute PESE-GC if the ensemble is not entirely degenerate
-                if np.abs( fcst_ens1d.max() - fcst_ens1d.min() ) > 1e-7:
+            # Only execute PESE-GC if the ensemble is not entirely degenerate
+            if np.abs( fcst_ens1d.max() - fcst_ens1d.min() ) > 1e-7:
 
-                    # -----------------------------------------------------------------------------------
-                    #    PESE CODE HERE
-                    # -----------------------------------------------------------------------------------
+                # -----------------------------------------------------------------------------------
+                #    PESE CODE HERE
+                # -----------------------------------------------------------------------------------
 
-                    # Step 1: fit MUWE distribution at current location
-                    #     Note that the user-specified distribution in muwe is the one specified in 
-                    #     config_CAM_pyPESE.py
-                    params = all_dist_class_dict['muwe'].fit(
-                        fcst_ens1d, all_dist_class_dict[user_dist_name]
+                # Step 1: fit MUWE distribution at current location
+                #     Note that the user-specified distribution in muwe is the one specified in 
+                #     config_CAM_pyPESE.py
+                params = all_dist_class_dict['muwe'].fit(
+                    fcst_ens1d, all_dist_class_dict[user_dist_name]
+                )
+                fitted_dist = all_dist_class_dict['muwe']( 
+                    *params
+                )
+
+                # Step 2: Transform to probit space
+                fcst_probit1d = std_norm_dist.ppf(
+                    fitted_dist.cdf( fcst_ens1d )
+                )
+
+                # Step 3: Resample in probit space
+                fcst_probit1d -= np.mean( fcst_probit1d)
+                fcst_probit1d /= np.std( fcst_probit1d, ddof=1)                   
+                virt_probit1d = np.matmul( fcst_probit1d[np.newaxis,:], E_matrix )
+                virt_probit1d = np.array(virt_probit1d)[0,:]
+
+                # Step 4: Transform from probit space to native space
+                virt_ens1d = fitted_dist.ppf(
+                    std_norm_dist.cdf( virt_probit1d )
+                )
+                virt_ens1d = virt_ens1d.astype('f8')
+
+                # Checking for strange values
+                if np.sum( np.isnan( virt_ens1d) + np.isinf(virt_ens1d) ) > 0:
+                    msg = "Strange virtual values for %s detected at (%d, %f, %f)" % (
+                        vname, ilvl,
+                        my_xy_ind_dict[gridtype]['y inds'][iloc], my_xy_ind_dict[gridtype]['x inds'][iloc]
                     )
-                    fitted_dist = all_dist_class_dict['muwe']( 
-                        *params
-                    )
-
-                    # Step 2: Transform to probit space
-                    fcst_probit1d = std_norm_dist.ppf(
-                        fitted_dist.cdf( fcst_ens1d )
-                    )
-
-                    # Step 3: Resample in probit space
-                    fcst_probit1d -= np.mean( fcst_probit1d)
-                    fcst_probit1d /= np.std( fcst_probit1d, ddof=1)                   
-                    virt_probit1d = np.matrix( fcst_probit1d[np.newaxis,:]) * E
-                    virt_probit1d = np.array(virt_probit1d)[0,:]
-
-                    # Step 4: Transform from probit space to native space
-                    virt_ens1d = fitted_dist.ppf(
-                        std_norm_dist.cdf( virt_probit1d )
-                    )
-                    virt_ens1d = virt_ens1d.astype('f8')
-
-                    # Checking for strange values
-                    if np.sum( np.isnan( virt_ens1d) + np.isinf(virt_ens1d) ) > 0:
-                        msg = "Strange virtual values for %s detected at (%d, %f, %f)" % (
-                            vname, ilvl,
-                            my_xy_ind_dict[gridtype]['y inds'][iloc], my_xy_ind_dict[gridtype]['x inds'][iloc]
-                        )
-                        msg += "\nfcst_ens1d: " + str( fcst_ens1d)
-                        msg += "\nfcst_probit1d: " + str( fcst_probit1d )
-                        msg += "\nvirt_probit1d: " + str( virt_probit1d )
-                        msg += "\nvirt_ens1d:"  + str( virt_ens1d)
-                        timed_print(msg)
-                        timed_print( virt_probit1d )
+                    msg += "\nfcst_ens1d: " + str( fcst_ens1d)
+                    msg += "\nfcst_probit1d: " + str( fcst_probit1d )
+                    msg += "\nvirt_probit1d: " + str( virt_probit1d )
+                    msg += "\nvirt_ens1d:"  + str( virt_ens1d)
+                    timed_print(msg)
+                    timed_print( virt_probit1d )
 
 
-                    # -----------------------------------------------------------------------------------
-                    #    PESE CODE ENDS HERE
-                    # -----------------------------------------------------------------------------------
-                    
-                # --- End of handling case where the ensemble is not entirely degenerate.
+                # -----------------------------------------------------------------------------------
+                #    PESE CODE ENDS HERE
+                # -----------------------------------------------------------------------------------
+                
+            # --- End of handling case where the ensemble is not entirely degenerate.
 
-                # Cases where the ensemble is essentially fully degenerate
-                if np.abs( fcst_ens1d.max() - fcst_ens1d.min() ) <= 1e-7:
-                    virt_ens1d = np.mean( fcst_ens1d )
-                # --- End of handling fully degenerate ensemble
+            # Cases where the ensemble is essentially fully degenerate
+            if np.abs( fcst_ens1d.max() - fcst_ens1d.min() ) <= 1e-7:
+                virt_ens1d = np.mean( fcst_ens1d )
+            # --- End of handling fully degenerate ensemble
 
-                # Put 1d data into virtual array
-                virt_vble_data[ix,:] = virt_ens1d
+            # Put 1d data into virtual array
+            virt_vble_data[ix,:] = virt_ens1d
 
-            # --- End of loop over available elements
+        # --- End of loop over available elements
 
-            # Undo dimension manipulations on virtual data
-            virt_dims = list( spatial_dims ).append( virt_ens_size )
-            virt_vble_data = np.swapaxes(
-                virt_vble_data.reshape( virt_dims ), -1, ens_dim_id
-            )
+        # Undo dimension manipulations on virtual data
+        virt_dims = list( spatial_dims ).append( virt_ens_size )
+        virt_vble_data = np.swapaxes(
+            virt_vble_data.reshape( virt_dims ), -1, ens_dim_id
+        )
 
-            # Hold onto virtual ensemble
-            virt_ens_dict[f'{tkey}lvl variables'][vname][data] = deepcopy(virt_vble_data)
+        # Hold onto virtual ensemble
+        virt_ens_dict[f'{tkey}lvl variables'][vname]['data'] = deepcopy(virt_vble_data)
 
-            gc_collect()
+        gc_collect()
 
-        # --- End of loop over variables within type
-    # --- End of checking whether to execute BPGC for this kind of variable
+    # --- End of loop over variables within type
+    timed_print(f'Finished applying PESE-GC onto {typekey} variables.\n')
 # --- End of loop over pressure and single-lvl variable types
 
 
 
 
-        
 
 
 
@@ -424,13 +429,76 @@ for typekey in ['pres', 'single']:
 
 
 
+'''
+    WRITE VIRTUAL MEMBERS INTO VIRTUAL ENSEMBLE FILE
+'''
+# Iterate over both kinds of ensemble data
+for typekey in ['pres', 'single']:
+
+    tkey = typekey[0]
+    vbl_config_dict = {'p': plvl_vbl_config_dict, 's': sngl_vbl_config_dict}[tkey]
+
+    # Skip if user doesnt want to expand the current type of data.
+    if not ens_config_dict[f'expand {typekey} lvl data?']:
+        continue
+
+    timed_print(f'Writing virtual ERA5 ensemble {typekey} lvl data to file.')
+
+    # Init virtual ensemble file
+    out_fname = ens_config_dict[f'virtual era5 {typekey} lvl file name']
+    virt_ncfile = ncopen( out_fname, 'w' )
+
+    # Open original data file
+    orig_ncfile = ncopen( 
+        ens_config_dict[f'original era5 {typekey} lvl file name'], 'r'
+    )
+
+    # Copy global attributes from original file to virtual file
+    virt_ncfile.setncatts(
+        {attr: orig_ncfile.getncattr(attr) for attr in orig_ncfile.ncattrs()}
+    )
+
+    # Copy all but the "number" dimension
+    for name, dimension in orig_ncfile.dimensions.items():
+        # Skip over number dimension
+        if name == 'number':
+            continue
+        # Actual copying process
+        virt_ncfile.createDimension(name, (len(dimension) if not dimension.isunlimited() else None))
+    # --- End of loop over non-number dimensions
+
+    # Generate new number dimension
+    virt_ncfile.createDimension('number', virt_ens_size)
+
+    # Save virtual members into netcdf file
+    for name, variable in fcst_ncfile.variables.items():
+
+        # Init variable.
+        x = virt_ncfile.createVariable(
+            name, variable.datatype, variable.dimensions
+        )
+        virt_ncfile[name][:] = (
+            virt_ens_dict[f'{tkey}lvl variables'][name]['data'][:] 
+        )
+        x.setncatts({k: variable.getncattr(k) for k in variable.ncattrs()})
+    
+    # --- End of loop over variables
+
+    # Save virtual members
+    virt_ncfile.close()
+
+    # Release file handle
+    orig_ncfile.close()
+
+    timed_print(f'Finished writing virtual ERA5 ensemble {typekey} lvl data to file.\n')
+
+# --- End of loop over data types
+timed_print()
 
 
-            
 
 
-
-
+    
 
 
 
